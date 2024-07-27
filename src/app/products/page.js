730cec9bfe540f96e18d259532e1e9d8cd20453d
BestOlumese <bestolumese@gmail.com>
2024-07-27 11:36:19 +0100

    Setup 2

---
+++ src/app/products/page.js
@@ -0,0 +1,8 @@
+export default function Products() {
+    return (
+      <div className="p-10">
+        <p className="text-center text-4xl font-semibold">Products Page</p>
+      </div>
+    );
+  }
+  
