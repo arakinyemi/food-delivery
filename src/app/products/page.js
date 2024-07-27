355d2c9f38ed7a211b6a27e7a2bc75596bdd8d28
BestOlumese <bestolumese@gmail.com>
2024-07-27 11:45:12 +0100

    branch commit

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,7 +1,7 @@
 export default function Products() {
     return (
       <div className="p-10">
-        <p className="text-center text-4xl font-semibold">Products Page branck</p>
+        <p className="text-center text-4xl font-semibold">Products Page</p>
       </div>
     );
   }
